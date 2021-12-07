4a7c4ab9e79316ffe082f5ac198e731730730426
Jerry <183437677@qq.com>
2021-12-07 14:06:45 -0500

    Add test PokemonFactory

--- src/test/java/usecase_pokemon/LevelCalculatorTest.java
+++ src/test/java/usecase_pokemon/LevelCalculatorTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.*;
 public class LevelCalculatorTest {
 
     @Test
-    public void calculateLevel() {
+    public void testCalculateLevel() {
         assertEquals(100, new LevelCalculator().calculateLevel(10000));
     }
 
     @Test
-    public void calculateExperiencePoint() {
+    public void testCalculateExperiencePoint() {
         assertEquals(10000, new LevelCalculator().calculateExperiencePoint(100));
     }
 }
