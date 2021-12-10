f89ef1f0c205e635fa53abb7263abfb02fb1b3f0
tony <tonyxichen@gmail.com>
2021-12-10 08:06:12 -0500

    doc change, added test for UserManager along with modified class that is for only testing.

---
+++ src/test/java/usecase_user/UserManagerTest.java
@@ -0,0 +1,41 @@
+package usecase_user;
+
+import usecase_user.UserReadWriterForTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Test;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.junit.Assert.*;
+
+public class UserManagerTest {
+    UserReadWriterForTest testURW = new UserReadWriterForTest();
+    UserManager testUM = new UserManager(testURW);
+
+    @Test
+    public void testHasUser() {
+        assertTrue(testUM.hasUser("testuser"));
+    }
+
+    @Test
+    public void testLogin() {
+        assertTrue(testUM.login("testuser", "testpassword"));
+    }
+
+    @Test
+    public void testRegister() throws IOException {
+        File file = new File("test_data/TestUserInfo.ser");
+        if (file.delete()) {
+            Path source = new File("test_data/backup/TestUserInfo.ser").toPath();
+            Path dest = new File("test_data/TestUserInfo.ser").toPath();
+            Files.copy(source, dest, REPLACE_EXISTING);
+            boolean b1 = testUM.register("register", "register");
+            boolean b2 = testUM.hasUser("register");
+            assertTrue(b1 && b2);
+        }
+    }
+}
---
+++ src/test/java/usecase_user/UserReadWriterForTest.java
@@ -0,0 +1,39 @@
+package usecase_user;
+
+import gateway.ConfigReader;
+import gateway.ReadWriter;
+import usecase_data.IReadWriter;
+
+import java.io.IOException;
+
+/**
+ * the data access object of user
+ */
+public class UserReadWriterForTest implements IReadWriter {
+    private final ReadWriter rw = new ReadWriter();
+
+    /**
+     *
+     * @return the user data
+     * @throws IOException file not exist exception
+     * @throws ClassNotFoundException class not exist exception
+     */
+    @Override
+    public Object read() throws IOException, ClassNotFoundException {
+        ConfigReader configReader = new ConfigReader();
+        String filePath = "test_data/TestUserInfo.ser";
+        return rw.read(filePath);
+    }
+
+    /**
+     *
+     * @param o object will be written
+     * @throws IOException file not exist
+     */
+    @Override
+    public void write(Object o) throws IOException {
+        ConfigReader configReader = new ConfigReader();
+        String filePath = "test_data/TestUserInfo.ser";
+        rw.write(filePath, o);
+    }
+}
